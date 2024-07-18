961bb6845c1125121aa5b617ab4ec4328e962f66
Kwabena Twumasi <ktwum721@gmail.com>
2024-07-18 13:48:33 +0000

    🔧 Everything is documented

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -42,10 +42,16 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Initialiazation methods
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Format method
+        """
         original_message = super().format(record)
         filtered_message = filter_datum(
                 self.fields, self.REDACTION,
