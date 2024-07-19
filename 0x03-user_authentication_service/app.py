11eedc995f4f58d465671eaae3c04d5dbcc3f50e
Kwabena Twumasi <ktwum721@gmail.com>
2024-07-19 01:09:00 +0000

    ✅ Session Cookies stored

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 A basic flask app
 """
-from flask import Flask, jsonify, Response, request
+from flask import Flask, jsonify, Response, request, abort
 from auth import Auth
 
 
@@ -38,6 +38,29 @@ def users() -> Response:
         return jsonify({
             "message": "email already registered"
         }), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> Response:
+    """
+    Login Users
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        return abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response = jsonify({
+        "email": email,
+        "message": "logged in"
+    })
+
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
