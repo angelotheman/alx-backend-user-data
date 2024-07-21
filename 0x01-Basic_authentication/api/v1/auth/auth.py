563980cd369ec9abde7186b6ac24d0415b9de2e9
Kwabena Twumasi <ktwum721@gmail.com>
2024-07-21 16:48:11 +0000

    ✅ Authentication folder added

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Authentication module for the application
+"""
+import re
+from flask import request
+from typing import List
+
+
+class Auth:
+    """
+    Class to handle authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Looks for the required paths
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Authorization header function
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user logged in
+        """
+        return None
