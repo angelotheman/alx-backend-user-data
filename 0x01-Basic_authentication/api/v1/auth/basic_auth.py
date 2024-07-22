966a43cba42c8a88b35d57e3c0f004573ecec48a
Kwabena Twumasi <ktwum721@gmail.com>
2024-07-22 12:39:18 +0000

    ✅ Task splited for max

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,7 +65,7 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return None, None
 
-        username, password = decoded_base64_authorization_header.split(':')
+        username, password = decoded_base64_authorization_header.split(':', 1)
 
         return username, password
 
